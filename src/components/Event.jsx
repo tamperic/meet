1c21c4c9a5d5bd7327bfbe484107b7ce84091b32
tamperic <tamaraperic.work@gmail.com>
2025-04-27 23:08:40 +0200

    Create Event component

---
+++ src/components/Event.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const Event = ({ event }) => {
+  const [isCollapsed, setIsCollapsed] = useState(true); 
+
+  if (!event) return null; // Return nothing if no event is passed
+
+  const handleToggleDetails  = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  return (
+    <div className="event">
+      <h2>{event.summary}</h2>
+      <p><strong>Location:</strong> {event.location}</p>
+      <p><strong>Start:</strong> {new Date(event.start.dateTime).toLocaleString()}</p>
+      {!isCollapsed && (
+        <>
+          <p><strong>End:</strong> {new Date(event.end.dateTime).toLocaleString()}</p>
+          <p><strong>Description:</strong> {event.description}</p>
+          <p><strong>{event.creator.email}</strong> has created this event on {new Date(event.created).toLocaleString()}</p>
+        </>
+      )}
+
+      <button onClick={handleToggleDetails}>{ isCollapsed ? 'More details' : 'Less details' }</button>
+    </div>
+  );
+};
+
+// PropTypes for validation
+Event.propTypes = {
+  event: PropTypes.shape({
+    summary: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    start: PropTypes.shape({
+      dateTime: PropTypes.string.isRequired,
+    }).isRequired,
+    end: PropTypes.shape({
+      dateTime: PropTypes.string.isRequired,
+    }).isRequired,
+    description: PropTypes.string.isRequired,
+    creator: PropTypes.shape({
+      email: PropTypes.string.isRequired,
+    }).isRequired,
+    created: PropTypes.string.isRequired,
+  })
+};
+
+
+export default Event;

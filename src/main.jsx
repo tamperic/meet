0f76265f4857d7d1481b58d1a0cc3e515aa41102
tamperic <tamaraperic.work@mail.com>
2025-05-08 10:31:19 +0200

    Import atatus & add “Test integration” code

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as atatus from 'atatus-spa';
+
+
+atatus.config('8444b7f7008b4a5b95bfe441cb37187d').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+atatus.notify(new Error('Test Atatus Setup'));

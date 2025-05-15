1e226c28e405755de12272bb8bf19cdd27ec6973
tamperic <tamaraperic.work@mail.com>
2025-05-15 21:58:17 +0200

    Update <XAxis>

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -21,7 +21,10 @@ const CityEventsChart = ({ events, allLocations }) => {
         <ResponsiveContainer width="99%" height={400}>
             <ScatterChart margin={{ top: 20, bottom: 20, left: 20, right: 20}}>
                 <CartesianGrid />
-                <XAxis type="category" dataKey="city" name="City" />
+                <XAxis 
+                    type="category" dataKey="city" name="City" 
+                    angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
+                />
                 <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
                 <Tooltip cursor={{ strokeDasharray: '3 3'}} />
                 <Scatter name="A school" data={data} fill="#8884d8" />
